c2a2f2f529e31932933ae6d04d8f12bb6cff4b57
Viktor Woll <Viktor_woll@hotmail.com>
2017-08-04 13:41:34 +0000

    kontakt mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
     default to: 'viktor_woll@hotmail.com'
     
-    def contact_email(name,email,body)
+    def contact_email(name, email, body)
       @name = name
       @email = email
       @body = body
